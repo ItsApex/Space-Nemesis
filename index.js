c869ee9326df5ac6ef8599b40755139725c92cc1
Aryan Singh <aryansingh6011@gmail.com>
2022-04-05 17:03:25 +0530

    all good

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
 
+
+
 canvas.width = 1024
 canvas.height = 576
 
--- js/classes.js
+++ js/classes.js
@@ -1,3 +1,7 @@
+
+
+
+
 class Sprite {
   constructor({
     position,
